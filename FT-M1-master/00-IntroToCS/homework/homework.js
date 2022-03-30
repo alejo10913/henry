84c010af1ffc44f2bd896413b437a7e919621bac
alejo10913 <joagonzalezfi@unal.edu.co>
2022-03-29 21:52:15 -0500

    homework 1 y 2

--- FT-M1-master/00-IntroToCS/homework/homework.js
+++ FT-M1-master/00-IntroToCS/homework/homework.js
@@ -1,13 +1,33 @@
 'use strict'
 
 function BinarioADecimal(num) {
-  // tu codigo aca
-
+  // pasar num a un array
+  //hacer un ciclo for para hacer la conversion 
+  // hacer sumatoria 
+var decimal = [...num].reverse();
+var suma = 0;
+for (var i = 0; i < decimal.length; i++){
+  suma = suma + decimal[i] * Math.pow(2, i);
 }
+return suma;
+}
+
 
 function DecimalABinario(num) {
   // tu codigo aca
+  //crear un array vacio 
+  // dividir numero con el modulo 
+  // añadir el resto al array
+  var binario = []
+  var resto;
+  do{
+    resto = (num % 2)
+    binario.push(resto);
+    num = Math.floor(num / 2);
 
+  }
+  while (num > 0);
+return binario.reverse().join("");
 }
 
 
